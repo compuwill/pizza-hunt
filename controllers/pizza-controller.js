dea40803abe50f915a866e05aad35ec28f2a3701
compuwill <wpvorngsam@hotmail.com>
2022-05-19 22:04:30 -0500

    added validation

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,38 +4,38 @@ const pizzaController = {
     // get all pizzas
     getAllPizza(req, res) {
         Pizza.find({})
-          .populate({
-            path: 'comments',
-            select: '-__v'
-          })
-          .select('-__v')
-          .sort({ _id: -1 })
-          .then(dbPizzaData => res.json(dbPizzaData))
-          .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-          });
-      },
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
 
     // get one pizza by id
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
-        .populate({
-            path: 'comments',
-            select: '-__v'
-        })
-        .select('-__v')
-        .then(dbPizzaData => {
-            if (!dbPizzaData) {
-            res.status(404).json({ message: 'No pizza found with this id!' });
-            return;
-            }
-            res.json(dbPizzaData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
     },
 
     // createPizza
@@ -48,7 +48,7 @@ const pizzaController = {
 
     // update pizza by id
     updatePizza({ params, body }, res) {
-        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
             .then(dbPizzaData => {
                 if (!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found with this id!' });
